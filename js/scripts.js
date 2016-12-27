42e597232e0c941e096e1d88f4d27f853f839f6e
blessb15 <blakebless7@gmail.com>
2016-12-27 10:09:32 -0800

    change scroll buttons on scroll.

--- js/scripts.js
+++ js/scripts.js
@@ -29,15 +29,22 @@ $(Document).ready(function() {
     $(links[0]).removeClass('link-blur');
   });
 
-  // for(var i = 0; i < links.length; i++){
-  //   $(links[i]).hover(function(){
-  //     console.log(i);
-  //   }, function(){
-  //     if(i == 0){
-  //       $(links[1]).removeClass('link-blur');
-  //       $(links[2]).removeClass('link-blur');
-  //     }
-  //   });
-  // }
+  var scrollPosit = $(document).scrollTop();
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() >= 0 && $(document).scrollTop() < 832){
+      $('.nav-bubble-contact').removeClass('active');
+      $('.nav-bubble-menu').removeClass('active');
+      $('.nav-bubble-home').addClass('active');
+    } else if ($(document).scrollTop() >= 832 && $(document).scrollTop() < 1682){
+      $('.nav-bubble-contact').removeClass('active');
+      $('.nav-bubble-home').removeClass('active');
+      $('.nav-bubble-menu').addClass('active');
+    } else {
+      $('.nav-bubble-menu').removeClass('active');
+      $('.nav-bubble-home').removeClass('active');
+      $('.nav-bubble-contact').addClass('active');
+    }
+  });
 
 });
